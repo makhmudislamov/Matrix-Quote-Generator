50316b35f06354ed847765a2aa46955db5ea8c47
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-11-28 21:33:13 -0800

    pseudocode

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -3,4 +3,9 @@ def rearrange():
     """
     Returns randomly rearranged words taken from the command line
     """
+    # create empty string or list to save user input
+    # take string input from command line
+    # iterate through the list/string
+    # and print randomly rearranged strings - always print all of the strings
+    # and always print different combinations
     pass
