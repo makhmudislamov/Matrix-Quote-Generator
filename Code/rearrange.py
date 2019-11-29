8c5e7cc0c0cba2c0fcba3c34bef6dafd607d2bea
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-11-28 22:06:47 -0800

    rearranging with Fisher-Yates shuffle

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -5,14 +5,15 @@ def rearrange(user_input):
     """
     Returns randomly rearranged words taken from the command line
     """
-    # create empty string or list to save user input
-    # take string input from command line
-    # iterate through the list/string
-    # and print randomly rearranged strings - always print all of the strings
-    # and always print different combinations
+    # Fisher-Yates shuffle
+    for indx in range(len(user_input)-1, 0, -1):
+        rand_indx = random.randint(0, indx)
+        # swapping
+        user_input[indx], user_input[rand_indx] = user_input[rand_indx], user_input[indx]
     print(user_input)
 
 
+
 if __name__ == '__main__':
-    user_input = sys.argv[1]
+    user_input = sys.argv[1:]
     rearrange(user_input)
