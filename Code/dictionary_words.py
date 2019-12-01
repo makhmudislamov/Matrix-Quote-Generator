dbf8e0d5400cdbcf690cefa96e0906c37c722625
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-11-30 22:57:08 -0800

    placed the words into list using split()

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,15 +1,23 @@
 
-def dictionary_words():
+def dictionary_words(word_amount=None):
 
+    
     # read in the words file
     with open('/usr/share/dict/words', 'r') as f:
         read_data = f.read()
+        # splits the words after each space
+        # and puts them into a list
+        splitted_data = read_data.split()
+    
+    
 
-    print(read_data)
+    
+
+
     # select a random set of words from the file and store in a data type
     # put the number of words requested together into a "sentence"
     # output your sentence
 
 
 if __name__ == '__main__':
-    pass
+    dictionary_words()
