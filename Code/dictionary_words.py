62a9801608af86059c635008fe6e16ad864f4e58
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-01 11:52:03 -0800

    bug - printing only first x amount of words

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,3 +1,4 @@
+import random
 
 def dictionary_words(word_amount=None):
 
@@ -7,9 +8,24 @@ def dictionary_words(word_amount=None):
         read_data = f.read()
         # splits the words after each space
         # and puts them into a list
-        splitted_data = read_data.split()
+    read_data_list = read_data.split()
     
-    # 
+    # from the list above choose random word_amount and print it as a sentence.
+    # example. user inputs 5
+    # functions should pick random 5 words and print it on the console with "." at the end
+    # we can use fisher-yates shuffle to pick random 5 words and print it on the console
+
+    # generating random index from 0 to len of list   
+    rand_indx = random.randint(0, len(read_data_list)-1)
+
+    # now limit the random word pick to word_count input by user
+    for rand_indx in range(0, word_amount + 1):
+
+        random_words = read_data_list[rand_indx]
+        print(random_words)
+
+
+
 
     
 
@@ -20,4 +36,4 @@ def dictionary_words(word_amount=None):
 
 
 if __name__ == '__main__':
-    dictionary_words()
+    dictionary_words(10)
