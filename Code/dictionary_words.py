1b4e11f3cabfcabb2d2e5e02f0bf849711f31318
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-11-30 22:27:42 -0800

    reading the dictionary

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,6 +1,15 @@
 
+def dictionary_words():
 
-# read in the words file
-# select a random set of words from the file and store in a data type
-# put the number of words requested together into a "sentence"
-# output your sentence
+    # read in the words file
+    with open('/usr/share/dict/words', 'r') as f:
+        read_data = f.read()
+
+    print(read_data)
+    # select a random set of words from the file and store in a data type
+    # put the number of words requested together into a "sentence"
+    # output your sentence
+
+
+if __name__ == '__main__':
+    pass
