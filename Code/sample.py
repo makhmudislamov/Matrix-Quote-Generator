2aef363c83f9badbbe1ece932302de5bbff71440
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-02 13:52:37 -0800

    setup for sample()

---
+++ Code/sample.py
@@ -0,0 +1,10 @@
+def sample(histogram):
+    """
+    takes a histogram (however you've structured yours) 
+    and returns a single word, at random. It should not yet 
+    take into account the distributions of the words
+    """
+    # iterate through the histogram
+    # return one random word
+    
+    pass
