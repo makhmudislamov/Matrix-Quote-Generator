cfc8a10fdca5a755201edd207a09844023a70bc5
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-01 16:02:07 -0800

    dicitonary_words - fully functioning

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,39 +1,24 @@
 import random
+import sys
 
 def dictionary_words(word_amount=None):
 
-    
+    output = []
     # read in the words file
     with open('/usr/share/dict/words', 'r') as f:
         read_data = f.read()
         # splits the words after each space
         # and puts them into a list
     read_data_list = read_data.split()
-    
-    # from the list above choose random word_amount and print it as a sentence.
-    # example. user inputs 5
-    # functions should pick random 5 words and print it on the console with "." at the end
-    # we can use fisher-yates shuffle to pick random 5 words and print it on the console
 
-   
-
-    # now limit the random word pick to word_count input by user
     for _ in range(0, word_amount):
          # generating random index from 0 to len of list
         rand_indx = random.randint(0, len(read_data_list)-1)
         random_words = read_data_list[rand_indx]
-        print(random_words)
-
-
-
-
-    
-
-
-    # select a random set of words from the file and store in a data type
-    # put the number of words requested together into a "sentence"
-    # output your sentence
-
+        output.append(random_words)
+    # TODO: small bug - fullstop 
+    print(*output,f".")
 
 if __name__ == '__main__':
-    dictionary_words(3)
+    word_amount = int(sys.argv[1])
+    dictionary_words(word_amount)
