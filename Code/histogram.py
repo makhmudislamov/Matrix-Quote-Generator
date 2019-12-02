ce09b750dcf069cce64e4de042690cf3dbc6e8a2
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-02 13:45:27 -0800

    frequency - works

--- Code/histogram.py
+++ Code/histogram.py
@@ -2,27 +2,28 @@ from cleaner import file_cleaner
 
 class Histogram(object):
     
-    def __init__(self, file):
-        self.file = file
-        self.hist = {}
+    def __init__(self):
+        
+        self.histogram = {}
 
-    def histogram(self, file):
+    def build(self, file):
         """
         Takes a source_text argument (can be either a filename or the contents of the file as a string, your choice) 
         and return a histogram
         data structure that stores each unique word along with 
         the number of times the word appears in the source text.
         """
-        
+        # cleaning the file and working with pure text 
         cleaned_text = file_cleaner(file)
+        print(cleaned_text)
         for word in cleaned_text:
             # checking if the word is not in the dict
-            if word not in self.hist.keys():
-                self.hist[word] = 1
+            if word not in self.histogram.keys():
+                self.histogram[word] = 1
             else:
-                self.hist[word] += 1
-
-        return self.hist
+                self.histogram[word] += 1
+        print(self.histogram)
+        return self.histogram
 
 
 
@@ -37,16 +38,18 @@ class Histogram(object):
         
         return unique_word_count
 
-    def frequency(self, word, histogram):
+    def frequency(self, word):
         """
         Takes a word and histogram argument and returns the number of times that word appears in a text
         """
-        pass
+        return self.histogram.get(word)
+        
+
+            
 
 
 if __name__ == '__main__':
     file = "./sample_words.txt"
-    hist = Histogram(file)
-    histogram = {'one': 3, 'fish': 6, 'two': 2,
-                 'red': 3, 'blue': 1, 'musor': 1}
-    print(hist.unique_words(histogram))
+    histogram = Histogram()
+    histogram.build(file)
+    print(histogram.frequency('fiLOLO'))
