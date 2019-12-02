dff042d4555abab8c910a94fa614c30b294b3669
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-01 20:41:38 -0800

    added corpus text and setup histogram.py

---
+++ Code/histogram.py
@@ -0,0 +1,27 @@
+
+
+class Histogram(object):
+    
+    def __init__(self):
+        pass
+
+    def histogram(self, source_text):
+        """
+        Takes a source_text argument (can be either a filename or the contents of the file as a string, your choice) 
+        and return a histogram
+        data structure that stores each unique word along with 
+        the number of times the word appears in the source text.
+        """
+        pass
+
+    def unique_words(self, histogram):
+        """
+        Takes a histogram argument and returns the total count of unique words in the histogram.
+        """
+        pass
+
+    def frequency(self, word, histogram):
+        """
+        Takes a word and histogram argument and returns the number of times that word appears in a text
+        """
+        pass
