674772c6b76213ecc7e6cc757f3f96c67c445df0
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-08 14:06:34 -0800

    bugfix = double iteration. markov chain is built

--- Code/markov_chain_one.py
+++ Code/markov_chain_one.py
@@ -31,7 +31,6 @@ def markov_chain_one(pure_text):
     for index in range(len(pure_text)-1):
         word = pure_text[index]
         
-
         # filling the markov chain dict
         if word not in markov_chain:
             markov_chain[word] = Dictogram([pure_text[index + 1]])
@@ -39,5 +38,6 @@ def markov_chain_one(pure_text):
         else:
             markov_chain[word].add_count([pure_text[index + 1]])
 
-    
+    return markov_chain
+
 print(markov_chain_one(pure_text))
