25ea9bb4f6c7b46aa4d724ed8028de88f16da1a6
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-08 16:48:22 -0800

    nth order markov chain

--- Code/markov_chain_one.py
+++ Code/markov_chain_one.py
@@ -1,5 +1,6 @@
 from cleaner import file_cleaner
 from dictogram import Dictogram
+import sample
 
 # cleaning the corpus and returning as a list of strings - also can be considered as tokenization
 pure_text = file_cleaner("./sample_words.txt")
@@ -27,10 +28,31 @@ def markov_chain_one(pure_text):
 
 # print(markov_chain_one(pure_text))
 
+def markov_chain_n_order(n_order, pure_text):
+
+    markov_chain = {}
+    # iterating over the list and want to use each word as key, value as nested dict for markov_chain
+    for index in range(len(pure_text)-n_order):
+        # pure_text[index] should be our word from list
+        sequence_range = tuple(pure_text[index: index + n_order])
+
+        # check if key is stored already
+        if sequence_range not in markov_chain:
+            #  create new entry with window as key and dictogram as value
+            markov_chain[sequence_range] = Dictogram([pure_text[index + n_order]])     
+        else:
+            # append it to the existing histogram
+            markov_chain[sequence_range].add_count([pure_text[index + n_order]])
+           
+    return markov_chain
+
+print(markov_chain_n_order(2, pure_text))
+
 def generate_sentence(m_chained_dict):
     """
     Takes markov chained dictionary and generates a sentence
     """
     output_sentence = []
     pass
+    
 
