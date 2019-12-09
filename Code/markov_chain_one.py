0453e2f236c120ae18f38aee9c4d3d299e4951d2
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-08 16:10:39 -0800

    started sentence generating func

--- Code/markov_chain_one.py
+++ Code/markov_chain_one.py
@@ -1,24 +1,9 @@
-"""
-Now let's try implementing this! You'll need to write code to do two things:
-
-1. Learn a Markov chain from a corpus. You've already written code to find how often a token appears in a corpus, but now you need to find how often a token appears after another token.
-
-2. Do a random walk on a Markov chain. This should be pretty simple if you pick a good way to store the Markov chain you learn.
-
-Make sure to think about what data structures to use to make your code efficient. Both learning the Markov chain and taking a step of the random walk should take time at most linear in the size of the corpus.
-
-When you're ready, try your implementation out on a real corpus and see how it compares to the stochastic sampler!
-"""
-
-
 from cleaner import file_cleaner
 from dictogram import Dictogram
 
 # cleaning the corpus and returning as a list of strings - also can be considered as tokenization
 pure_text = file_cleaner("./sample_words.txt")
-print(pure_text)
-
-
+# print(pure_text)
 
 def markov_chain_one(pure_text):
     """
@@ -40,4 +25,12 @@ def markov_chain_one(pure_text):
 
     return markov_chain
 
-print(markov_chain_one(pure_text))
+# print(markov_chain_one(pure_text))
+
+def generate_sentence(m_chained_dict):
+    """
+    Takes markov chained dictionary and generates a sentence
+    """
+    output_sentence = []
+    pass
+
