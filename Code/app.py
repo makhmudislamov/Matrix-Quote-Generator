669eb9f8c91c502c68d6fe2195b22e4831925bd2
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-10 14:32:05 -0800

    updated flask configuration

--- Code/app.py
+++ Code/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from sample import *
 from markov_chain_one import *
+from cleaner import file_cleaner
 app = Flask(__name__)
 
 
 @app.route('/')
-def markov():    
-    final = generate_sentence(20)
+def markov():
+    filename = './corpus.txt'
+    pure_text = file_cleaner(filename)
+    markov_chain = markov_chain_n_order(4, pure_text)
+    final = generate_sentence(markov_chain, 20)
     return final
 
 
