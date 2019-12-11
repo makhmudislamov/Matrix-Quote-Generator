1fb1cc816294b75ebdb56d10f4187b00fdaa1216
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-11 13:36:27 -0800

    adding UI to flask

--- Code/app.py
+++ Code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sample import *
 from markov_chain_one import *
 from cleaner import file_cleaner
@@ -7,11 +7,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def markov():
-    filename = './corpus.txt'
-    pure_text = file_cleaner(filename)
-    markov_chain = markov_chain_n_order(4, pure_text)
-    final = generate_sentence(markov_chain, 20)
-    return final
+    # filename = './corpus.txt'
+    # pure_text = file_cleaner(filename)
+    # markov_chain = markov_chain_n_order(4, pure_text)
+    # final = generate_sentence(markov_chain, 20)
+    # return final
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
