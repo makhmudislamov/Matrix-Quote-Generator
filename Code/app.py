aa9fa3f3276227dfc34555234404b0051b8068b6
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-15 15:35:26 -0800

    retrieved words are showing

--- Code/app.py
+++ Code/app.py
@@ -11,19 +11,27 @@ structure = autocomplete_setup(vocabulary)
 
 @app.route('/', methods=['POST', 'GET'])
 def get_prefix():
+    """
+    should take prefix input
+    then it should redirect to /words route
+    """  
+    return render_template('home.html')
+
+
+@app.route('/words', methods=['POST', 'GET'])
+def autocompleted_words():
+    """
+    Show all autocompleted words to choose from for loop should be here 
+    also here we should have a form to POST chosen words then it should redirect to /quote
+    """
     # getting user input = prefix
     if request.method == 'POST':
         prefix = request.form['prefix']
         # pasting prefix to autocomplete()
         words_to_choose = autocomplete(prefix, structure)
         print("generated words", words_to_choose)
-    
-    return render_template('home.html')
 
-
-@app.route('/words', methods=['POST', 'GET'])
-def funcname():
-    return render_template('retrieved.html')
+    return render_template('retrieved.html', words=words_to_choose)
 
 @app.route('/quote', methods=['POST', 'GET'])
 def quote():
